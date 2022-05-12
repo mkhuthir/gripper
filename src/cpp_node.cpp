784d6a1fc5783128aab4098b9c5306b185bc8289
mkhuthir <mkhuthir@gmail.com>
2022-05-12 20:57:31 +0400

    u

---
+++ src/cpp_node.cpp
@@ -0,0 +1,12 @@
+#include "gz_ros2_control/cpp_header.cpp"
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("my_node");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
